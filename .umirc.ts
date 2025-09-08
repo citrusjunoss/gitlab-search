ce5a964eebc90b704c67423716ae1a852fccaba2
橙 <chengjunchao@hotmail.com>
2025-09-08 20:54:48 +0800

    fix: 自动部署

--- .umirc.ts
+++ .umirc.ts
@@ -9,6 +9,8 @@ export default defineConfig({
   layout: {
     title: '@umijs/max',
   },
+  base: '/global-search/', // 设置应用的基础路径
+  publicPath: '/global-search/', // 设置静态资源的公共路径
   proxy: {
     '/api/v4': {
       target: 'https://gitlab.qizhidao.com/',
