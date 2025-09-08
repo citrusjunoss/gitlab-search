39ff3c8d26411a1ec3337c44602adab6db1142ed
橙 <chengjunchao@hotmail.com>
2025-09-08 21:06:00 +0800

    fix: 自动部署

--- .umirc.ts
+++ .umirc.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   layout: {
     title: '@umijs/max',
   },
+  esbuildMinifyIIFE: true, // 解决 esbuild helpers 冲突问题
   exportStatic: {}, // 启用静态导出，为每个路由生成独立的 HTML 文件
   base: '/global-search/', // 设置应用的基础路径
   publicPath: '/global-search/', // 设置静态资源的公共路径
