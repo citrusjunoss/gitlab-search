d50a3e5c442a9bb4736b0dfed64b0b78239a828b
橙 <chengjunchao@hotmail.com>
2025-09-08 21:02:32 +0800

    fix: 自动部署

--- .umirc.ts
+++ .umirc.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   layout: {
     title: '@umijs/max',
   },
+  exportStatic: {}, // 启用静态导出，为每个路由生成独立的 HTML 文件
   base: '/global-search/', // 设置应用的基础路径
   publicPath: '/global-search/', // 设置静态资源的公共路径
   proxy: {
